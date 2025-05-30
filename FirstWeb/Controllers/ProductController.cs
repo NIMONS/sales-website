a9174bcf8a403373c56d5639128dac6ec359c141
Nimons <phuc61204@gmail.com>
2025-05-30 17:50:57 +0700

    chi tiết sản phẩm

--- FirstWeb/Controllers/ProductController.cs
+++ FirstWeb/Controllers/ProductController.cs
@@ -1,17 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirstWeb.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FirstWeb.Controllers
 {
 	public class ProductController : Controller
 	{
+		private readonly DataContext _dataContext;
+
+		public ProductController(DataContext dataContext)
+		{
+			this._dataContext = dataContext;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
 		}
 
-		public IActionResult Details()
+		public async Task<IActionResult > Details(int id)
 		{
-			return View();
+			if(id == null) return RedirectToAction("Index");
+
+			var productById = this._dataContext.Products.Where(c => c.Id == id).FirstOrDefault();
+
+
+			return View(productById);
 		}
 	}
 }
