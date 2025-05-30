789f7d631dc7e591e32e7cf8938f6ac1408968f8
Nimons <phuc61204@gmail.com>
2025-05-30 17:10:13 +0700

    sản phẩm theo thương hiệu

---
+++ FirstWeb/Controllers/BrandController.cs
@@ -0,0 +1,26 @@
+﻿using FirstWeb.Models;
+using FirstWeb.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstWeb.Controllers
+{
+	public class BrandController : Controller
+	{
+		private readonly DataContext _dataContext;
+
+		public BrandController(DataContext dataContext)
+		{
+			_dataContext = dataContext;
+		}
+
+		public async Task<IActionResult > Index(string slug = "")
+		{
+			BrandModel brand = this._dataContext.Brands.Where(c => c.Slug == slug).FirstOrDefault();	
+			if (brand == null) return RedirectToAction("Index");
+			var productsByBrand = this._dataContext.Products.Where(c => c.BrandId == brand.Id);
+
+			return View(await productsByBrand.OrderByDescending(c => c.Id).ToListAsync());
+		}
+	}
+}
