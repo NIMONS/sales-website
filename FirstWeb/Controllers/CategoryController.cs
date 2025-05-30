ea16409d8201423c08e8d59d7df1d3533ef72f81
Nimons <phuc61204@gmail.com>
2025-05-30 17:01:18 +0700

    sản phẩm theo danh mục

--- FirstWeb/Controllers/CategoryController.cs
+++ FirstWeb/Controllers/CategoryController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirstWeb.Models;
+using FirstWeb.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FirstWeb.Controllers
 {
 	public class CategoryController : Controller
 	{
-		public IActionResult Index()
+		private readonly DataContext _dataContext;
+		public CategoryController(DataContext dataContext)
 		{
-			return View();
+			this._dataContext = dataContext;
+		}
+
+		public async Task<IActionResult> Index(string slug = "")
+		{
+			CategoryModel category = this._dataContext.Categories.Where(c => c.Slug == slug).FirstOrDefault();
+			if (category == null) return RedirectToAction("Index");
+			var productsByCategory = this._dataContext.Products.Where(c => c.CategoryID == category.Id);
+
+			return View(await productsByCategory.OrderByDescending(c=> c.Id).ToListAsync());
 		}
 	}
 }
