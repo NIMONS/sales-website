d13d195d208815e8d0a82e147a9f6a8416b7fbea
Nimons <phuc61204@gmail.com>
2025-05-30 16:48:25 +0700

    hiển thị sản phẩm

--- FirstWeb/Controllers/HomeController.cs
+++ FirstWeb/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 using System.Diagnostics;
 using FirstWeb.Models;
+using FirstWeb.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FirstWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataContext _dataContext;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext context)
         {
             _logger = logger;
+            this._dataContext = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var produtcs = this._dataContext.Products.Include("Category").Include("Brand").ToList();
+            return View(produtcs);
         }
 
         public IActionResult Privacy()
