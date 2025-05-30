ca3843f72af911ae84e0117b4e73633d741ed1ed
Nimons <phuc61204@gmail.com>
2025-05-30 16:29:21 +0700

    View component Sidebar

---
+++ FirstWeb/Repository/Components/BrandsViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstWeb.Repository.Components
+{
+	public class CategoriesViewComponent : ViewComponent
+	{
+		private readonly DataContext _dataContext;
+
+		public CategoriesViewComponent(DataContext dataContext)
+		{
+			this._dataContext = dataContext;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync() => View(await this._dataContext.Categories.ToListAsync());
+	}
+}
---
+++ FirstWeb/Repository/Components/CategoriesViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstWeb.Repository.Components
+{
+	public class BrandsViewComponent : ViewComponent
+	{
+		private readonly DataContext _dataContext;
+
+		public BrandsViewComponent(DataContext dataContext)
+		{
+			this._dataContext = dataContext;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync() => View(await this._dataContext.Brands.ToListAsync());
+	}
+}
