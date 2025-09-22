30458f63e43004b98ac8cf679f72474048a10216
Nimons <phuc61204@gmail.com>
2025-09-22 15:21:53 +0700

    complete function add category in admin

--- FirstWeb/Areas/Admin/Controllers/CategoryController.cs
+++ FirstWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,11 @@
-﻿using FirstWeb.Repository;
+﻿using FirstWeb.Models;
+using FirstWeb.Repository;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+using System.Web;
 
 namespace FirstWeb.Areas.Admin.Controllers
 {
@@ -22,6 +27,53 @@ namespace FirstWeb.Areas.Admin.Controllers
 			return View(listCategories);
 		}
 
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(CategoryModel category)
+		{
+
+			category.Description = HttpUtility.HtmlDecode(category.Description);
+			category.Description = Regex.Replace(category.Description, "<.*?>", string.Empty);
+
+			if (ModelState.IsValid)
+			{
+				category.Slug = category.Name.Replace(" ", "-");
+				var slug = await this._dataContext.Categories.FirstOrDefaultAsync(p => p.Slug == category.Slug);
+				if (slug != null)
+				{
+					ModelState.AddModelError("", "Danh mục đã có trong database");
+					return View(category);
+				}
+
+				this._dataContext.Add(category);
+				await this._dataContext.SaveChangesAsync();
+				TempData["success"] = "Thêm danh mục thành công";
+
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				TempData["error"] = "model bị thiếu giá trị";
+				List<string> errors = new List<string>();
+				foreach (var value in ModelState.Values)
+				{
+					foreach (var error in value.Errors)
+					{
+						errors.Add(error.ErrorMessage);
+					}
+				}
+
+				string errorMessage = string.Join("\n", errors);
+				return BadRequest(errorMessage);
+			}
+
+		}
 
 	}
 }
--- FirstWeb/Models/CategoryModel.cs
+++ FirstWeb/Models/CategoryModel.cs
@@ -7,13 +7,12 @@ namespace FirstWeb.Models
 		[Key]
 		public int Id { get; set; }
 
-		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập tên loại sản phẩm")]
+		[Required(ErrorMessage = "Yêu cầu nhập tên loại sản phẩm")]
 		public string Name { get; set; }
 
-		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập mô tả loại sản phẩm")]
+		[Required(ErrorMessage = "Yêu cầu nhập mô tả loại sản phẩm")]
 		public string Description { get; set; }
 
-		[Required]
 		public string Slug { get; set; }
 
 		public int Status { get; set; }
